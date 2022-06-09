5126c7f961646d1f78022e72cb83c2e3325f9532
ed <c_cstudy@126.com>
2022-06-09 13:17:16 +0800

    20220609

--- add_commit_push.bash
+++ add_commit_push.bash
@@ -4,7 +4,7 @@ INFO=$(date +%Y%m%d)
 
 #git status
 #echo;echo
-git add  -A * && echo add_succese
+git add *  * && echo add_succese
 git commit -m $INFO && echo commit_succese
 git push -u origin master && echo git_push_succese.
 echo;echo
